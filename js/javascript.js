63ce2979403332222a6f7710bab79c713fbc1dbb
harryfisherdesign <harrymichael@hotmail.co.nz>
2014-10-05 12:15:25 +1300

    Website Files

---
+++ js/javascript.js
@@ -0,0 +1,59 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	$('footer h3').hide();
+	$('.introtitle').hide();
+        $('footer h3').fadeIn(2000);
+		$('.introtitle').fadeIn(2000);
+		
+		
+		$(document).one('keypress', function(){  
+			$('footer img:nth-child(1)').animate({bottom:'-400px'},500);
+			$('footer img:nth-child(2)').animate({bottom:'+5px'},200);
+			$('footer img:nth-child(2)').animate({bottom:'-400px'},500);
+			$('footer img:nth-child(3)').animate({bottom:'+9px'},200);
+			$('footer img:nth-child(3)').animate({bottom:'-400px'},500);
+			$('footer h3').animate({bottom:'+20px'},200);
+			$('footer h3').animate({bottom:'-400px'},500);
+			$('.introtitle').fadeOut(500);
+			
+		});
+		
+$(document).one('keypress', function(){  
+$('body').delay(700).animate({backgroundColor: '#c9e3b5'},500)  
+$('.footermain').delay(500).animate({bottom: '-250px'},500)
+$('.blogbutton').delay(500).animate({top: '0px'},500)
+$('.searchgardensbutton').delay(800).animate({top: '0px'},500)
+$('.homebutton').delay(650).animate({top: '0px'},500)
+$('.mainlogo').delay(1000).animate({top: '200px'},300)
+$('.mainlogo').animate({top: '125px'},150)
+$('.mainlogo').animate({top: '200px'},150)
+
+$('.loginbar').delay(1100).animate({top: '450px'},300)
+$('.loginbar').animate({top: '400px'},150)
+$('.loginbar').animate({top: '450px'},150)
+
+$('.loginbuttonbar').delay(1150).animate({top: '485px'},300)
+$('.loginbuttonbar').animate({top: '435px'},150)
+$('.loginbuttonbar').animate({top: '485px'},150)
+
+
+});
+
+$('.footermain img').click(function(event) {
+	event.preventDefault();
+	if($(this).hasClass('isDown')){
+	$('.footermain').animate({bottom: '+10px'},300).animate({bottom: '-250px'},500);
+	$(this).removeClass('isDown');
+	} else {
+		$('.footermain').animate({bottom: '+10px'},500).animate({bottom: '0px'},300);
+		$(this).addClass('isDown');	
+	}
+		return false;
+});
+
+});
+
+
+
+
---
+++ js/javascriptotherpages.js
@@ -0,0 +1,154 @@
+// JavaScript Document
+
+$(document).ready(function() {
+		
+		
+$('.searchbar').animate({top: '400px'},500)
+$('.searchbar').animate({top: '350px'},150)
+$('.searchbar').animate({top: '400px'},150)
+$('.searchbuttonbar').delay(50).animate({top: '480px'},500)
+$('.searchbuttonbar').animate({top: '430px'},150)
+$('.searchbuttonbar').animate({top: '480px'},150)
+
+$('.growingtrees img:nth-child(1)').delay(300).animate({top: '0px'},1100);
+$('.growingtrees img:nth-child(2)').delay(400).animate({top: '5px'},900);
+$('.growingtrees img:nth-child(3)').delay(600).animate({top: '20px'},1000);
+
+$('.signuptree').animate({bottom:'0px'},700);
+$('.details').delay(300).animate({bottom: '35%'},700);
+$('.signupbutton').delay(500).animate({bottom: '20%'},700);
+
+
+$('.footerprofile').animate({bottom: '0px'},500)
+$('.mask').animate({bottom: '0px'},700)
+$('.footermain').animate({bottom: '0px'},700)
+$('.blogbutton').animate({top: '0px'},500)
+$('.progressionbutton').animate({top: '0px'},500)
+$('.searchgardensbutton').delay(300).animate({top: '0px'},500)
+$('.adjustcamerabutton').delay(375).animate({top: '0px'},500)
+$('.homebutton').delay(150).animate({top: '0px'},500)
+$('.fullscreenbutton').delay(225).animate({top: '0px'},500)
+$('.pot').delay(500).animate({bottom: '0px'},500)
+$('.dirt').delay(500).animate({bottom: '250px'},500)
+$('.optionplant').delay(550).animate({bottom: '-50px'},1000)
+
+
+$('.link1').delay(50).animate({top: '270px'},500)
+$('.link1').animate({top: '250px'},150)
+$('.link1').animate({top: '270px'},150)
+
+$('.link2').delay(100).animate({top: '270px'},500)
+$('.link2').animate({top: '200px'},150)
+$('.link2').animate({top: '270px'},150)
+
+$('.link3').delay(75).animate({top: '270px'},500)
+$('.link3').animate({top: '200px'},150)
+$('.link3').animate({top: '270px'},150)
+
+
+/*FULL SCREEN*/
+$('.fullscreenbutton').click(function() {
+	$('.exitfullscreen').animate({top: '0px'},500)
+	$('.footerprofile').animate({bottom: '-300px'},700)
+	$('.mask').animate({bottom: '-300px'},700)
+	$('.blogbutton').animate({top: '-1000px'},500)
+	$('.progressionbutton').animate({top: '-100px'},500)
+	$('.searchgardensbutton').delay(300).animate({top: '-100px'},500)
+	$('.adjustcamerabutton').delay(375).animate({top: '-100px'},500)
+	$('.homebutton').delay(150).animate({top: '-100px'},500)
+	$('.fullscreenbutton').delay(225).animate({top: '-100px'},500)
+	$('.pot').delay(300).animate({bottom: '-290px'},500)
+	$('.dirt').delay(300).animate({bottom: '-40px'},500)
+	$('.optionplant').delay(300).animate({bottom: '-450px'},500)
+});
+
+$('.exitfullscreen img').hover(function(event) {
+	event.preventDefault();
+	if($(this).hasClass('nothovering')){
+	$('.exitfullscreen img').animate({opacity: '0.3'},500);
+	$(this).removeClass('nothovering');
+	} 
+else {
+		$('.exitfullscreen img').animate({opacity: '1'},500);
+		$(this).addClass('nothovering');	
+	}
+		return false;
+});
+
+/*Exit Full Screen*/
+$('.exitfullscreen').click(function() {
+	$('.footerprofile').animate({bottom: '0px'},500)
+$('.mask').animate({bottom: '0px'},700)
+$('.footermain').animate({bottom: '0px'},700)
+$('.blogbutton').animate({top: '0px'},500)
+$('.progressionbutton').animate({top: '0px'},500)
+$('.searchgardensbutton').delay(300).animate({top: '0px'},500)
+$('.adjustcamerabutton').delay(375).animate({top: '0px'},500)
+$('.homebutton').delay(150).animate({top: '0px'},500)
+$('.fullscreenbutton').delay(225).animate({top: '0px'},500)
+$('.pot').delay(300).animate({bottom: '0px'},500)
+$('.dirt').delay(300).animate({bottom: '250px'},500)
+$('.optionplant').delay(300).animate({bottom: '-50px'},500)
+$('.exitfullscreen').animate({top:'-100px'},500)
+
+});
+
+
+$('.plantmenuwrap').hover(function(event) {
+	event.preventDefault();
+	if($(this).hasClass('isDown')){
+	$('.optionplant').animate({bottom: '-50px'},500);
+	$(this).removeClass('isDown');
+	} 
+else {
+		$('.optionplant').animate({bottom: '250px'},500);
+		$(this).addClass('isDown');	
+	}
+		return false;
+});
+
+
+$('.link1').hover(function(event) {
+	event.preventDefault();
+	if($(this).hasClass('isDown')){
+	$('.tumblrtree').animate({top: '200px'},500);
+	$(this).removeClass('isDown');
+	} else {
+		$('.tumblrtree').animate({top: '-80px'},500);
+		$(this).addClass('isDown');	
+	}
+		return false;
+});
+
+
+$('.link2').hover(function(event) {
+	event.preventDefault();
+	if($(this).hasClass('isDown')){
+	$('.facebooktree').animate({top: '200px'},500);
+	$(this).removeClass('isDown');
+	} else {
+		$('.facebooktree').animate({top: '-80px'},500);
+		$(this).addClass('isDown');	
+	}
+		return false;
+});
+
+$('.link3').hover(function(event) {
+	event.preventDefault();
+	if($(this).hasClass('isDown')){
+	$('.twittertree').animate({top: '200px'},500);
+	$(this).removeClass('isDown');
+	} else {
+		$('.twittertree').animate({top: '-80px'},500);
+		$(this).addClass('isDown');	
+	}
+		return false;
+});
+
+});
+
+
+
+
+
+
