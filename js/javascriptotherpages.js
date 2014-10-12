047becc7407708fca9e7536f484c7cce9215e590
harryfisherdesign <harrymichael@hotmail.co.nz>
2014-10-13 03:57:11 +1300

    GPD07

--- js/javascriptotherpages.js
+++ js/javascriptotherpages.js
@@ -145,6 +145,33 @@ $('.link3').hover(function(event) {
 		return false;
 });
 
+
+var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'oXWMt4vkQ80',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
 });
 
 
